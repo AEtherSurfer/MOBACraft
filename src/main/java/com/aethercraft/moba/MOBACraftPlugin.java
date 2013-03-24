10c8cf1d30088fba1def73d3026948b45bd13661
AEtherSurfer <aethersurfer@aethercraft.com>
2013-03-23 22:23:31 -0400

    file name capitalization step 1 of 2

--- src/main/java/com/aethercraft/moba/MOBACraftPlugin.java
+++ src/main/java/com/aethercraft/moba/MOBACraftPlugin.java
@@ -4,7 +4,7 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class MOBACraft extends JavaPlugin {
+public class MOBACraftPlugin extends JavaPlugin {
 
 	private Listener mobaListener;
 
